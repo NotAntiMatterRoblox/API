971343f338da120aa68078188ed4cc4fa3c4787d
NotAntiMatterRoblox <65586474+NotAntiMatterRoblox@users.noreply.github.com>
2020-06-10 01:13:28 -0700

    Update and rename SynX Library.lua to functionlist.lua

---
+++ Library/functionlist.lua
@@ -0,0 +1,42 @@
+[[
+Clipboard Library Support:
+API.clipboard_get
+API.clipboard_set
+
+Environment Functions Support: 
+API.getgenv
+API.getrenv
+API.getsenv
+API.getmenv
+API.getreg
+API.getobjects
+API.getgc
+API.getinstances
+API.getnilinstances
+API.getloadedmodules
+API.getconnections
+API.getrawmetatable
+API.getcallingscript
+API.getrunningscripts
+API.getscriptfunction
+API.gettenv
+API.getmetatable
+API.getupvalues
+API.getupvalue
+API.setupvalue
+API.getconstants
+API.getconstant
+API.setconstant
+API.upvaluename
+API.context_set
+API.context_get
+
+IO Library Support:
+
+API.io_write
+API.io_read
+API.io_append
+
+Drawing Library Support:
+API.Drawing_new
+]]
